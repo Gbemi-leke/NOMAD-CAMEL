38f991767f13dfb481b36d257985e0be9ea481b7
Gbemi-leke <leke.olamide123@gmail.com>
2025-07-27 22:54:40 +0100

    Update made

--- nomad_camel/settings.py
+++ nomad_camel/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'nomad_camel.middleware.DatabaseErrorMiddleware',
 ]
 
 ROOT_URLCONF = 'nomad_camel.urls'
