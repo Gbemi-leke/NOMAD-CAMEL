ca82b08f9b5ebce6b990d09364f114cb516c205d
Bussyboo98 <74764895+Bussyboo98@users.noreply.github.com>
2025-08-27 11:20:26 +0100

    pulling

--- nomad_camel/settings.py
+++ nomad_camel/settings.py
@@ -70,8 +70,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_app_password'
+EMAIL_HOST_USER = 'nomadcamelngpb@gmail.com'
+EMAIL_HOST_PASSWORD = 'rwfbghsdydemvktu'
 
 TEMPLATES = [
     {
